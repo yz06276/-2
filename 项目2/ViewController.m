2d8df371653395c91149649b0e4d70c8bdc9fa74
cb <437046579@qq.com>
2015-08-19 10:22:06 +0800

    Initial Commit

---
+++ 项目2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  项目2
+//
+//  Created by ios on 15/8/19.
+//  Copyright (c) 2015年 ios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 项目2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  项目2
+//
+//  Created by ios on 15/8/19.
+//  Copyright (c) 2015年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
